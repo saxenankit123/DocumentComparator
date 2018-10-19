3420c96d614adc89b600ec7698847116a7327ad1
saxenankit123 <37994077+saxenankit123@users.noreply.github.com>
2018-10-19 16:11:40 +0530

    Delete auto_nuget.bat

--- DocumentComparator/auto_nuget.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Auto building document comparator ------------------------------
-nuget pack DocumentComparator.nuspec
-echo Build complete                    ------------------------------
